64bb57b5c00cf6d65061fe331a49c90084c75669
Brett Tanonaka <tano.brett@gmail.com>
2023-03-08 11:22:02 -0800

    FormatList has basic formatting, no sample data yet

--- 1-glossary/client/src/components/FormatList.jsx
+++ 1-glossary/client/src/components/FormatList.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const FormatList = () => {
+const FormatList = ({ list, setList }) => {
+  let listItems = list.map(word => {
+    <li key={word.word}>{word.word}: {word.description}</li>
+  })
 
+  return (
+    <ol>{listItems}</ol>
+  )
 }
 
 export default FormatList;
