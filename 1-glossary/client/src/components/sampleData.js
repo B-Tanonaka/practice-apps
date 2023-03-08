c030ad7994bfa6fb961be569c3805930c7f0eb56
Brett Tanonaka <tano.brett@gmail.com>
2023-03-08 11:24:59 -0800

    Sample data created

---
+++ 1-glossary/client/src/components/sampleData.js
@@ -0,0 +1,8 @@
+let sampleData = [
+  {hello: 'saying wassup'},
+  {goodbye: 'saying get outta here'},
+  {howzit: 'greeting for saying hello'},
+  {choke: 'plenty, a lot'},
+  {guarens: 'guarantee'}
+  {cherreh: 'nice, awesome, crisp'}
+]
