5415038dd125649e07e78a969dad251760bb5802
Brett Tanonaka <tano.brett@gmail.com>
2023-03-08 11:09:04 -0800

    Created list components and passed down to addWords function

---
+++ 1-glossary/client/src/components/addWords.jsx
@@ -0,0 +1,12 @@
+
+
+const AddWords ({ list, setList }) => {
+
+
+  return (
+
+  )
+};
+
+
+export default AddWords;
--- 1-glossary/client/src/index.jsx
+++ 1-glossary/client/src/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { render } from "react-dom";
+import { render, useState } from "react-dom";
+import AddWords from "./components/addWords.jsx";
+
+const [list, setList] = useState([]);
+const [search, setSearch] = useState('');
 
 render(
   <div>
-    <p>Hello, World!</p>
+    <h1>Glossary</h1>
+    <AddWords list={list} setList={setList}/>
   </div>,
   document.getElementById("root")
 );
