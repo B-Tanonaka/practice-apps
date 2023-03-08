d9403923a8943dbabe081894091d759bd2bf7cd5
Brett Tanonaka <tano.brett@gmail.com>
2023-03-08 11:13:42 -0800

    Created FormatList function

---
+++ 1-glossary/client/src/components/FormatList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FormatList = () => {
+
+}
+
+export default FormatList;
--- 1-glossary/client/src/index.jsx
+++ 1-glossary/client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, useState } from "react-dom";
-import AddWords from "./components/addWords.jsx";
+import AddWords from "./components/AddWords.jsx";
+import FormatList from "./components/FormatList.jsx";
 
 const [list, setList] = useState([]);
 const [search, setSearch] = useState('');
@@ -8,7 +9,8 @@ const [search, setSearch] = useState('');
 render(
   <div>
     <h1>Glossary</h1>
-    <AddWords list={list} setList={setList}/>
+    <AddWords list={list} setList={setList} />
+    <FormatList list={list} setList={setList} />
   </div>,
   document.getElementById("root")
 );
