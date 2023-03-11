deefa296806602846bd87f5c85763b6ca8b4077e
Brett Tanonaka <tano.brett@gmail.com>
2023-03-10 22:26:37 -0800

    Drafted axios functions from front-end to server

--- 2-checkout/client/src/components/App.jsx
+++ 2-checkout/client/src/components/App.jsx
@@ -1,10 +1,42 @@
-import react from 'react';
+import react, { useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
 
+  const [totalForm, setTotalForm] = {{}};
+  const [F1, setF1] = useState({});
+  const [F2, setF2] = useState({});
+  const [F3, setF3] = useState({});
+
+  const getEntry = () => {
+    axios.get('/checkout')
+    .then(data => {setTotalForm(data)})
+    .catch(err => console.log(err))
+  }
+
+  const addEntry = (totalForm) => {
+    axios.post('/checkout')
+    .catch(err => console.log(err))
+  }
+
+  const editEntry = (totalForm) => {
+    getEntry()
+    .then(axios.patch('/checkout'))
+    .catch(err => console.log(err))
+  }
+
+  const deleteEntry = (totalForm) => {
+    axios.delete('/checkout', {data: totalForm})
+    .catch(err => console.log(err))
+  }
+
 
   return (
-    <div>where is it</div>
+    <div>
+      <F1 />
+      <F2 />
+      <F3 />
+    </div>
   )
 }
 
